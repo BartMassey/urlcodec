d90f060584e58aad499637af87d1aa9cfcfe55ed
Bart Massey <bart.massey@gmail.com>
2024-12-02 13:30:38 -0800

    removed useless -e argument, made default

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
 mod urlcodec;
 use urlcodec::*;
 
-//use std::io::{Read, Write};
 use std::collections::HashSet;
 
-use clap::{Parser, CommandFactory};
+use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
-    #[arg(short, long, help="urlencode")]
-    encode: bool,
     #[arg(short, long, help="urldecode")]
     decode: bool,
 }
 
 fn urlcodec() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    if !(args.encode ^ args.decode) {
-        return Err(Args::command().error(
-            clap::error::ErrorKind::ArgumentConflict,
-            "must specify either encode or decode (but not both)",
-        ).into());
-    }
 
     let preserve: HashSet<char> = ('a'..='z')
         .chain('A'..='Z')
@@ -31,10 +22,10 @@ fn urlcodec() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut stdin = std::io::stdin().lock();
     let mut stdout = std::io::stdout().lock();
-    if args.encode {
-        url_encode_stream(preserve, &mut stdin, &mut stdout)?;
-    } else {
+    if args.decode {
         url_decode_stream(&mut stdin, &mut stdout)?;
+    } else {
+        url_encode_stream(preserve, &mut stdin, &mut stdout)?;
     }
     Ok(())
 }
